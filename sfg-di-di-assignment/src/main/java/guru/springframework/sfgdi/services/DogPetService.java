e2f8419a81db9ca91674817905bc44f17ffe3380
Marc Fearby <marc.fearby@service.nsw.gov.au>
2022-05-08 20:15:09 +1000

    Dependency injection assignment attempt

---
+++ sfg-di-di-assignment/src/main/java/guru/springframework/sfgdi/services/DogPetService.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jt on 12/28/19.
+ */
+@Profile({"dog", "default"})
+@Service("dog")
+public class DogPetService {
+    public String getPetType(){
+        return "Dogs are the best!";
+    }
+}
