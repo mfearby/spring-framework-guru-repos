0f2a8ea7671b06710c3793835889f63af75458ee
Marc Fearby <marc.fearby@service.nsw.gov.au>
2022-05-08 20:25:44 +1000

    Fix issues I didn't notice

--- sfg-di-di-assignment/src/main/java/guru/springframework/sfgdi/services/DogPetService.java
+++ sfg-di-di-assignment/src/main/java/guru/springframework/sfgdi/services/DogPetService.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Service;
 /**
  * Created by jt on 12/28/19.
  */
-@Profile({"dog", "default"})
-@Service("dog")
-public class DogPetService {
+@Profile("dog")
+@Service
+public class DogPetService implements PetService {
     public String getPetType(){
         return "Dogs are the best!";
     }
