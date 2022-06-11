62e6cade985f5bdd9521e7137bc79cfae24c2964
Marc Fearby <marc.fearby@service.nsw.gov.au>
2022-06-11 22:30:55 +1000

    Merge .properties files (use the one automatically parsed by Spring Boot)

--- sfg-di-di-assignment/src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ sfg-di-di-assignment/src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -9,7 +9,9 @@ import guru.springframework.sfgdi.services.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
+// Moved these to application.properties (which is automatically parsed by Spring _BOOT_)
+// @PropertySource("classpath:datasource.properties")
+
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
